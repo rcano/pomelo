0261ea8290cd4f92050bd15b55e28cdb1748ef22
rcano <ioniviil@gmail.com>
2017-07-20 15:18:18 -0300

    less boilerplate

--- src/main/scala/pomelo/LiveEditionHeuristics.scala
+++ src/main/scala/pomelo/LiveEditionHeuristics.scala
@@ -28,39 +28,35 @@ object LiveEditionHeuristics {
       case _ => None
     }
   }
-  val closingPair: Heuristic = {
-    ca => consume(
-      keyTyped({ 
-          case "{"|"("|"["|"<"|"\""|"'"|"`" => true
-          case other => false
-        }: Predicate[String], KeyCombination.SHIFT_ANY, KeyCombination.ALT_ANY,KeyCombination.SHORTCUT_ANY),
-      { evt =>
-        val ClosingPair(c) = evt.getCharacter.charAt(0)
-        val pos = ca.getCaretPosition
-        ca.insertText(pos, evt.getCharacter + c)
-        ca.moveTo(pos + 1)
-      }: Consumer[KeyEvent])
-  }
+  val closingPair: Heuristic = ca => consume(
+    keyTyped({ 
+        case "{"|"("|"["|"<"|"\""|"'"|"`" => true
+        case other => false
+      }: Predicate[String], KeyCombination.SHIFT_ANY, KeyCombination.ALT_ANY,KeyCombination.SHORTCUT_ANY),
+    { evt =>
+      val ClosingPair(c) = evt.getCharacter.charAt(0)
+      val pos = ca.getCaretPosition
+      ca.insertText(pos, evt.getCharacter + c)
+      ca.moveTo(pos + 1)
+    }: Consumer[KeyEvent])
   
-  val indentation: Heuristic = { 
-    ca => consume(
-      keyPressed(KeyCode.ENTER),
-      { evt =>
-        val currentLine = ca.getParagraph(ca.getCurrentParagraph).getText
-        if (currentLine.isEmpty) {
-          ca.insertText(ca.getCaretPosition, "\n")
-        } else {
-          val indent = currentLine.iterator.takeWhile(' '.==).size
-          val indentText = " " * indent
-          val sb = new StringBuilder().append("\n").append(indentText)
-          val shouldIncreaseIdent = currentLine.charAt(ca.getCaretColumn - 1) == '{'
-          if (shouldIncreaseIdent) sb.append("  \n").append(indentText)
-          ca.insertText(ca.getCaretPosition, sb.toString)
-          if (shouldIncreaseIdent) {
-            ca.lineStart(SelectionPolicy.CLEAR)
-            ca.previousChar(SelectionPolicy.CLEAR)
-          }
+  val indentation: Heuristic = ca => consume(
+    keyPressed(KeyCode.ENTER),
+    { evt =>
+      val currentLine = ca.getParagraph(ca.getCurrentParagraph).getText
+      if (currentLine.isEmpty) {
+        ca.insertText(ca.getCaretPosition, "\n")
+      } else {
+        val indent = currentLine.iterator.takeWhile(' '.==).size
+        val indentText = " " * indent
+        val sb = new StringBuilder().append("\n").append(indentText)
+        val shouldIncreaseIdent = currentLine.charAt(ca.getCaretColumn - 1) == '{'
+        if (shouldIncreaseIdent) sb.append("  \n").append(indentText)
+        ca.insertText(ca.getCaretPosition, sb.toString)
+        if (shouldIncreaseIdent) {
+          ca.lineStart(SelectionPolicy.CLEAR)
+          ca.previousChar(SelectionPolicy.CLEAR)
         }
-      }: Consumer[KeyEvent])
-  }
+      }
+    }: Consumer[KeyEvent])
 }

57167efcd207d901d4bfe91474ca29309f2ee301
rcano <ioniviil@gmail.com>
2017-07-20 15:58:32 -0300

    fix index out of bounds exception in indentation heuristic. added ignore closing pair heuristic to be symmetric with the auto generation of closing pair.

--- src/main/scala/pomelo/LiveEditionHeuristics.scala
+++ src/main/scala/pomelo/LiveEditionHeuristics.scala
@@ -4,7 +4,8 @@ import java.util.function.{Consumer, Predicate}
 import javafx.scene.input.{KeyCode, KeyEvent, KeyCombination}
 import org.fxmisc.richtext.CodeArea
 import org.fxmisc.richtext.model.NavigationActions.SelectionPolicy
-import org.fxmisc.wellbehaved.event.InputMap, InputMap.consume
+import org.fxmisc.wellbehaved.event.InputHandler
+import org.fxmisc.wellbehaved.event.InputMap, InputMap._
 import org.fxmisc.wellbehaved.event.EventPattern._
 
 object LiveEditionHeuristics {
@@ -13,28 +14,26 @@ object LiveEditionHeuristics {
   
   def All: Seq[Heuristic] = Seq(
     closingPair,
-    indentation
+    indentation,
+    ignoreClosingPair,
+    deleteClosingPair
   )
   
-  private object ClosingPair {
-    def unapply(c: Char) = c match {
-      case '{' => Some("}")
-      case '(' => Some(")")
-      case '[' => Some("]")
-      case '<' => Some(">")
-      case '"' => Some("\"")
-      case '\'' => Some("'")
-      case '`' => Some("`")
-      case _ => None
-    }
-  }
   val closingPair: Heuristic = ca => consume(
     keyTyped({ 
         case "{"|"("|"["|"<"|"\""|"'"|"`" => true
         case other => false
-      }: Predicate[String], KeyCombination.SHIFT_ANY, KeyCombination.ALT_ANY,KeyCombination.SHORTCUT_ANY),
+      }: Predicate[String], KeyCombination.SHIFT_ANY, KeyCombination.ALT_ANY, KeyCombination.SHORTCUT_ANY),
     { evt =>
-      val ClosingPair(c) = evt.getCharacter.charAt(0)
+      val c = evt.getCharacter.charAt(0) match {
+        case '{' => '}'
+        case '(' => ')'
+        case '[' => ']'
+        case '<' => '>'
+        case '"' => '"'
+        case '\'' => '\''
+        case '`' => '`'
+      }
       val pos = ca.getCaretPosition
       ca.insertText(pos, evt.getCharacter + c)
       ca.moveTo(pos + 1)
@@ -50,7 +49,7 @@ object LiveEditionHeuristics {
         val indent = currentLine.iterator.takeWhile(' '.==).size
         val indentText = " " * indent
         val sb = new StringBuilder().append("\n").append(indentText)
-        val shouldIncreaseIdent = currentLine.charAt(ca.getCaretColumn - 1) == '{'
+        val shouldIncreaseIdent = ca.getCaretColumn > 0 && currentLine.charAt(ca.getCaretColumn - 1) == '{'
         if (shouldIncreaseIdent) sb.append("  \n").append(indentText)
         ca.insertText(ca.getCaretPosition, sb.toString)
         if (shouldIncreaseIdent) {
@@ -58,5 +57,40 @@ object LiveEditionHeuristics {
           ca.previousChar(SelectionPolicy.CLEAR)
         }
       }
+    }: Consumer[KeyEvent])
+  
+  val ignoreClosingPair: Heuristic = ca => process(
+    keyTyped({ 
+        case "}"|")"|"]"|">"|"\""|"'"|"`" => true
+        case other => false
+      }: Predicate[String], KeyCombination.SHIFT_ANY, KeyCombination.ALT_ANY, KeyCombination.SHORTCUT_ANY),
+    { evt =>
+      val currentLine = ca.getParagraph(ca.getCurrentParagraph).getText
+      if (currentLine.length != 0 && currentLine.length > ca.getCaretColumn && 
+          currentLine.charAt(ca.getCaretColumn) == evt.getCharacter.charAt(0)) {
+        ca.nextChar(SelectionPolicy.CLEAR) //just move the cursor, instead of adding a character
+        InputHandler.Result.CONSUME
+      } else {
+        InputHandler.Result.PROCEED
+      }
+    }: java.util.function.Function[KeyEvent, InputHandler.Result])
+  
+  val deleteClosingPair: Heuristic = ca => consume(
+    keyPressed(KeyCode.BACK_SPACE),
+    { evt => 
+      val currentLine = ca.getParagraph(ca.getCurrentParagraph).getText
+      if (currentLine.length != 0 && ca.getCaretColumn > 0 && currentLine.length > ca.getCaretColumn) {
+        (currentLine.charAt(ca.getCaretColumn - 1), currentLine.charAt(ca.getCaretColumn)) match {
+          case ('{', '}') |
+            ('(', ')') |
+            ('[', ']') |
+            ('<', '>') |
+            ('"', '"') |
+            ('\'', '\'') |
+            ('`', '`') => ca.deleteNextChar()
+          case _ =>
+        }
+      }
+      ca.deletePreviousChar()
     }: Consumer[KeyEvent])
 }
